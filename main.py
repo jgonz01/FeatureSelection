09b648495d9b258b179b6be63ed422b12ba80336
jessicag <jessicag@jpl.nasa.gov>
2018-12-12 18:12:09 -0800

    working on fwd selection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from math import sqrt
 
 arr_data = []
 features = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -24,23 +24,57 @@ def nearest_neighbor(subset):
     """This function returns how many instances were guessed correctly."""
 
     subset_list = list(subset)
-    for instance in arr_data:
+    total_correct = 0
+    for i in range(len(arr_data)):
+        #print('i: ' + str(i))
         shortest_distance = 9999999999999999999999.0
-        for feature in subset_list:
-            distance +=
+        shortest_index = i
+        for j in range(len(arr_data)):
+            if j == i:
+                continue
+            #print('j: ' + str(j))
+            dist = 0
+            for feature in subset_list:
+                dist += pow(arr_data[i][feature] - arr_data[j][feature], 2)
+            dist = sqrt(dist)
+            if dist < shortest_distance:
+                shortest_distance = dist
+                shortest_index = j
+        if arr_data[i][0] == arr_data[shortest_index][0]:
+            total_correct += 1
+
+    return total_correct/num_instances
+
 
 def forward():
     """This function handles forward selection."""
 
     subset = set()
-    best = None
+    best_accuracy = 0.0
+    best_subset = set()
     for i in range(1, len(arr_data[0])):
-        print(i)
+        print('i: ' + str(i))
+        accuracy = 0.0
+        best_set = set()
         for j in range(1, len(arr_data[0])):
-            #accuracy = 0.0
-            subset = subset & set([j])
-            nearest_neighbor(subset)
+            if i == j and len(subset) != 0:
+                continue
+            print('j: ' + str(j))
+            temp_set = subset.copy()
+            temp_set.add(j)
+            print('set curr: ' + str(temp_set))
+            calc_accuracy = nearest_neighbor(temp_set)
+            print('accuracy: ' + str(accuracy))
+            if calc_accuracy > accuracy:
+                accuracy = calc_accuracy
+                best_set = temp_set
             #print('set: ' + str(subset))
+        subset = best_set
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_subset = subset
+
+    print('best features: ' + str(best_subset) + ' accuracy: ' + str(best_accuracy))
 
 
 if __name__ == "__main__":
@@ -58,11 +92,11 @@ if __name__ == "__main__":
 
     forward()
 
-    if algorithm == '1':
-        print('1')
-    elif algorithm == '2':
-        print("2")
-    elif algorithm == '3':
-        print("3")
+    # if algorithm == '1':
+    #     #print('1')
+    # elif algorithm == '2':
+    #     #print("2")
+    # elif algorithm == '3':
+    #     #print("3")
 
 
